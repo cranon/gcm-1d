b421b4faf4ecf99bae31cad8c37f10c84de10272
Alexander Kazakov <alexanderkazak@yandex.ru>
2013-09-23 17:20:48 +0400

    modified:   launcher.py

--- launcher.py
+++ launcher.py
@@ -5,7 +5,7 @@ import shutil
 import sys
 import math
 
-# Script for running cpp programm and making video
+# Script for running cpp programm and making video from plots
 
 if (sys.argv[1] == '-n'):
 	NumT = int(sys.argv[2])
