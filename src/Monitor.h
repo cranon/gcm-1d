916a79164956a0ffc4a96923286a38c3303c81b6
cranon <cranon@rambler.ru>
2013-09-25 19:18:20 +0400

    New class - Monitor

---
+++ src/Monitor.h
@@ -0,0 +1,15 @@
+#include "Mesh.h"
+using namespace std;
+
+class Monitor {
+	public:
+		float tau;
+		string rheology;
+		Monitor();
+		Monitor(int _Num, float _tau);
+		Monitor(int _Num, float _tau, string _rheology);
+		struct MonStruct monStruct;
+		void getH(Mesh *mesh);
+		void getCourant(Mesh *mesh);
+		~Monitor();
+};
